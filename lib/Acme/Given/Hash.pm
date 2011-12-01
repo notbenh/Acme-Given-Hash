07df571ba9b3d7eb89d8794be769d003b54ad486
ben hengst <ben.hengst@gmail.com>
2011-12-01 01:53:49 -0800

    first blush and it works

--- lib/Acme/Given/Hash.pm
+++ lib/Acme/Given/Hash.pm
@@ -1,5 +1,26 @@
+package Acme::Given::Hash;
 use strict;
 use warnings;
-package Acme::Given::Hash;
+use Exporter qw{import};
+our @EXPORT = qw{gvn};
+
+#ABSTRACT: is given() too much typing for you?
+
+sub gvn ($) {
+  my $when = shift;
+  die 'gvn requires a hashref' unless ref($when) eq 'HASH';
+  return bless $when, 'Acme::Given::Hash::Object';
+}
+
+package Acme::Given::Hash::Object;
+use strict;
+use warnings;
+
+use overload '~~' => sub{ 
+  my ($self, $key) = @_;
+  return ref($self->{$key}) eq 'CODE' 
+       ? $self->{$key}->() 
+       : $self->{$key};
+};
 
 1;
---
+++ t/00_works.t
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl 
+use strict;
+use warnings;
+
+use Test::More qw{no_plan};
+
+require_ok q{Acme::Given::Hash};
+use Acme::Given::Hash;
+
+is 'this' ~~ gvn { this => 'that'} , 'that', q{works!};
+is 'that' ~~ gvn { that => do{1+1}}, 2     , q{works!};
+is 'that' ~~ gvn { that => sub{3}} , 3     , q{works!};
+
+
